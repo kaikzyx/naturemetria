189d37c2854390acf9ff0fd014b92321ee74216f
Kaique Silva <kaikzyx@gmail.com>
2025-06-01 14:33:50 -0300

    refactor: add valid instance verification on camera target

--- source/camera/camera.gd
+++ source/camera/camera.gd
@@ -3,7 +3,9 @@ class_name Camera extends Camera2D
 @export var target: Node2D
 
 func _ready() -> void:
-	global_position = target.global_position
+	if is_instance_valid(target):
+		global_position = target.global_position
 
 func _process(delta: float) -> void:
-	global_position = global_position.lerp(target.global_position, 5 * delta)
+	if is_instance_valid(target):
+		global_position = global_position.lerp(target.global_position, 5 * delta)
