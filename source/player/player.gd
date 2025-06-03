649a8f700662209d60441386c6fea7f2043598dd
Kaique Silva <kaikzyx@gmail.com>
2025-06-02 22:10:41 -0300

    feat: add player death and exit game after death

--- source/player/player.gd
+++ source/player/player.gd
@@ -3,6 +3,9 @@ class_name Player extends CharacterBody2D
 @onready var _sprite: Sprite2D = $Sprite
 @onready var _state_machine: StateMachine = $StateMachine
 
+const _DEAD_FREEZE_TIME := 0.5
+const _DEAD_KNOCKBACK := 250.0
+const _DEAD_GRAVITY_FORCE := 500.0
 var speed := 100.0
 var damping := 10.0
 var direction := 1
@@ -32,7 +35,7 @@ func _on_hitbox_area_entered(area: Area2D) -> void:
 	area.owner.queue_free()
 
 func _on_hurtbox_body_entered(_body: Node2D) -> void:
-	queue_free()
+	_state_machine.request_state(&"dead_freeze")
 
 #region State machine callbacks.
 
@@ -68,5 +71,30 @@ func _on_fall_state_physics_updated(delta: float) -> void:
 	_system_gravity(delta)
 
 	if is_on_floor(): _state_machine.request_state(&"idle" if _get_movement_direction() == 0 else &"run")
+
+func _on_dead_freeze_state_entered() -> void:
+	get_tree().paused = true
+
+	# Configure player death.
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	collision_layer = 0
+	collision_mask = 0
+	velocity = Vector2.ZERO
+	z_index = RenderingServer.CANVAS_ITEM_Z_MAX
+	_sprite.texture = preload("res://assets/player_death.png")
+
+	# Wait for the timer and the real state of death begins.
+	await get_tree().create_timer(_DEAD_FREEZE_TIME).timeout
+	_state_machine.request_state(&"dead")
+
+func _on_dead_state_entered() -> void:
+	velocity.y = -_DEAD_KNOCKBACK
+
+func _on_dead_state_physics_updated(delta: float) -> void:
+	velocity.y += _DEAD_GRAVITY_FORCE * delta
+
+	# Exits the game when the player disappears from the screen.
+	if global_position.y > get_viewport_rect().size.y:
+		get_tree().quit()
 
 #endregion
