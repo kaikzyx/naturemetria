ee33d5cb72dfbfbe5a8e65701a067f39fb2838bb
Kaique Silva <kaikzyx@gmail.com>
2025-06-18 02:10:58 -0300

    feat: add more tiles, improvements and expand tile map

--- source/world/world.gd
+++ source/world/world.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
 @onready var _word_ui: WordUI = $WordUI
+@onready var _paticles: GPUParticles2D = $LeafParticles
 
 const _KILLED_SCORE_LABEL_OFFSET := Vector2(0, -16.0)
 const _CONSUMED_SCORE_LABEL_OFFSET := Vector2(0, -32.0)
+
+func _ready() -> void:
+	var size := get_viewport_rect().size
+	(_paticles.process_material as ParticleProcessMaterial).emission_box_extents = Vector3(size.x, size.y, 0.0)
+
+func _process(_delta: float) -> void:
+	_paticles.global_position = get_viewport().get_camera_2d().global_position
 
 func _on_main_ready(main: Main) -> void:
 	main.interface.add_child(_word_ui)
